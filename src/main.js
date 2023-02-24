df701c9ad8e842bdc0c2bdf06bd8bd0cffef7f30
HmadyGalo <helenmg1998@gmail.com>
2023-02-24 12:12:50 -0600

    Componente Finalizado

--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
 import { createApp } from "vue";
-  
+import App from "./App.vue";
+import BookInfo from "./components/BookInfo.vue";
+import "./assets/main.css";
+
+const app = createApp(App);
+app.component("book-info", BookInfo);
+app.mount("#app");
